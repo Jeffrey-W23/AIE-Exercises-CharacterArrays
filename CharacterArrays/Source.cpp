92e21c553fec5ca1c5f01ac8987c5b7dc510f09b
Thomas Wiltshire <thomaswiltshire@Kochanski.local>
2017-03-01 15:35:46 +1100

    First Commit.

---
+++ CharacterArrays/Source.cpp
@@ -0,0 +1,89 @@
+// Question 1 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	char FirstName[48];
+//	char LastName[48];
+//
+//	cout << "First name: ";
+//	cin >> FirstName;
+//
+//	cout << "Last name: ";
+//	cin >> LastName;
+//
+//	char FullName[128];
+//	strcpy(FullName, FirstName);
+//	strcat(FullName, " ");
+//	strcat(FullName, LastName);
+//
+//	cout << FullName << endl;
+//
+//	system("pause");
+//}
+// Question 1 //
+
+// Question 2 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	char FirstName[48];
+//	char LastName[48];
+//
+//	cout << "First name: ";
+//	cin >> FirstName;
+//
+//	cout << "Last name: ";
+//	cin >> LastName;
+//
+//	char FullName[128];
+//	strcpy(FullName, LastName);
+//	strcat(FullName, " ");
+//	strcat(FullName, FirstName);
+//
+//	cout << FullName << endl;
+//
+//	system("pause");
+//}
+// Question 2 //
+
+// Question 3 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	char Name[5][48];
+//
+//	cout << "First name 1: ";
+//	cin >> Name[0];
+//	cout << "First name 2: ";
+//	cin >> Name[1];
+//	cout << "First name 3: ";
+//	cin >> Name[2];
+//	cout << "First name 4: ";
+//	cin >> Name[3];
+//	cout << "First name 5: ";
+//	cin >> Name[4];
+//
+//	for (int i = 0; i < 5; i++)
+//	{
+//		char FullName[128];
+//		strcpy(FullName, Name[4]);
+//		strcat(FullName, Name[3]);
+//		strcat(FullName, Name[2]);
+//		strcat(FullName, Name[1]);
+//		strcat(FullName, Name[0]);
+//		cout << FullName << endl;
+//	}
+//
+//	system("pause");
+//}
+// Question 3 //
+
+// Question 5 //
+
+// Question 5 // 
